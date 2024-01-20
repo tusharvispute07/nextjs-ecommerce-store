beda4cfb9e9542a44087c0eb83fe18a6a23c337c
Tushar Vispute <tusharvispute07@gmail.com>
2024-01-20 14:18:34 +0530

    add log

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -23,6 +23,7 @@ export default async function handler(req, res){
   switch (event.type) {
     case 'checkout.session.completed':
       const data = event.data
+      console.log(data)
       const orderId = data.metadata.orderId
       const paid = data.payment_status === 'paid'
       if (orderId && paid){
